0a8ab4de0255c57ce829aa36a16243a187c095b4
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-31 01:14:41 +0600

    LP-119 promotions migration create

---
+++ database/migrations/2023_10_31_190551_create_promotions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('enabled')->nullable();
+            $table->foreignId('creator_id')->nullable();
+            $table->foreignId('editor_id')->nullable();
+            $table->foreignId('destroyer_id')->nullable();
+            $table->foreignId('restorer_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->timestamp('restored_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promotions');
+    }
+};
