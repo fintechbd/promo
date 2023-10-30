9fcc317607bd63e7741ca8a500cc9308d8b5981d
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-31 00:56:23 +0600

    LP-119 call promotion service

--- src/Promo.php
+++ src/Promo.php
@@ -2,6 +2,12 @@
 
 namespace Fintech\Promo;
 
+use Fintech\Promo\Services\PromotionService;
+
 class Promo
 {
+    public function promotion()
+    {
+        return app(PromotionService::class);
+    }
 }
