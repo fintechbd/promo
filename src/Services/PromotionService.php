f74c87fd84d4c2a21d633be7320854784aaa61f3
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-31 03:31:07 +0600

    LP-119 PHPDoc comment signature complete

--- src/Services/PromotionService.php
+++ src/Services/PromotionService.php
@@ -3,9 +3,11 @@
 namespace Fintech\Promo\Services;
 
 use Fintech\Promo\Interfaces\PromotionRepository;
+use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class PromotionService
+ * @property PromotionRepository $promotionRepository
  */
 class PromotionService
 {
@@ -18,45 +20,76 @@ class PromotionService
     }
 
     /**
+     * @param array $filters
      * @return mixed
      */
-    public function list(array $filters = [])
+    public function list(array $filters = []): mixed
     {
         return $this->promotionRepository->list($filters);
 
     }
 
-    public function create(array $inputs = [])
+    /**
+     * @param array $inputs
+     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
+     */
+    public function create(array $inputs = []): \Illuminate\Database\Eloquent\Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->promotionRepository->create($inputs);
     }
 
-    public function find($id, $onlyTrashed = false)
+    /**
+     * @param $id
+     * @param $onlyTrashed
+     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
+     */
+    public function find($id, $onlyTrashed = false): Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->promotionRepository->find($id, $onlyTrashed);
     }
 
-    public function update($id, array $inputs = [])
+    /**
+     * @param $id
+     * @param array $inputs
+     * @return Model|\MongoDB\Laravel\Eloquent\Model|null
+     */
+    public function update($id, array $inputs = []): Model|\MongoDB\Laravel\Eloquent\Model|null
     {
         return $this->promotionRepository->update($id, $inputs);
     }
 
-    public function destroy($id)
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function destroy($id): mixed
     {
         return $this->promotionRepository->delete($id);
     }
 
-    public function restore($id)
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function restore($id): mixed
     {
         return $this->promotionRepository->restore($id);
     }
 
-    public function export(array $filters)
+    /**
+     * @param array $filters
+     * @return mixed
+     */
+    public function export(array $filters): mixed
     {
         return $this->permissionRepository->list($filters);
     }
 
-    public function import(array $filters)
+    /**
+     * @param array $filters
+     * @return mixed
+     */
+    public function import(array $filters): mixed
     {
         return $this->permissionRepository->create($filters);
     }
