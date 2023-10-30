1157a3a8c2276f4f0b2c13a0fbd032f6f20625dc
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-10-31 01:34:31 +0600

    LP-119 promotion_type filter add

--- src/Repositories/Mongodb/PromotionRepository.php
+++ src/Repositories/Mongodb/PromotionRepository.php
@@ -4,6 +4,7 @@ namespace Fintech\Promo\Repositories\Mongodb;
 
 use Fintech\Core\Repositories\MongodbRepository;
 use Fintech\Promo\Interfaces\PromotionRepository as InterfacesPromotionRepository;
+use Fintech\Promo\Models\Promotion;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Collection;
 use InvalidArgumentException;
@@ -16,7 +17,7 @@ class PromotionRepository extends MongodbRepository implements InterfacesPromoti
 {
     public function __construct()
     {
-        $model = app(config('fintech.promo.promotion_model', \Fintech\Promo\Models\Promotion::class));
+        $model = app(config('fintech.promo.promotion_model', Promotion::class));
 
         if (! $model instanceof Model) {
             throw new InvalidArgumentException("Mongodb repository require model class to be `MongoDB\Laravel\Eloquent\Model` instance.");
@@ -42,6 +43,10 @@ class PromotionRepository extends MongodbRepository implements InterfacesPromoti
             } else {
                 $query->where('promotion_title', 'like', "%{$filters['search']}%");
             }
+        }
+
+        if (isset($filters['promotion_type']) && ! empty($filters['promotion_type'])) {
+            $query->where('promotion_type', $filters['promotion_type']);
         }
 
         //Display Trashed
