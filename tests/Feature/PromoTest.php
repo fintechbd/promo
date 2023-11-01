7f89fdbb43db41540ca3665c0076861c8e186456
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2023-11-01 13:13:59 +0600

    LP-119 Promotional event method create for test

---
+++ tests/Feature/PromoTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model as MYSQLDBLEBUPAY;
+use Illuminate\Support\Str;
+use MongoDB\Laravel\Eloquent\Model as MONGODB;
+
+use function Pest\Laravel\deleteJson;
+use function Pest\Laravel\getJson;
+use function Pest\Laravel\postJson;
+use function Pest\Laravel\putJson;
+
+/**
+ * @return MYSQLDBLEBUPAY|MONGODB|null
+ */
+function createPromotionEvent(): MYSQLDBLEBUPAY|MONGODB|null
+{
+    return \Fintech\Promo\Facades\Promo::promotion()->create([
+        'present_country_id' => 1,
+        'permanent_country_id' => 1,
+        'name' => Str::random(20),
+        'type' => 'event',
+        'content' => Str::random(200),
+        'link' => '',
+        'promotion_data' => [
+        ],
+        'enabled' => '1',
+    ]);
+}
+
