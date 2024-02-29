fb9a0b74a3ef9272d7b62190973c66dca739f732
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-03-01 02:10:34 +0600

    transaction repo updated

--- src/Repositories/Eloquent/PromotionRepository.php
+++ src/Repositories/Eloquent/PromotionRepository.php
@@ -16,13 +16,7 @@ class PromotionRepository extends EloquentRepository implements InterfacesPromot
 {
     public function __construct()
     {
-        $model = app(config('fintech.promo.promotion_model', Promotion::class));
-
-        if (!$model instanceof Model) {
-            throw new InvalidArgumentException("Eloquent repository require model class to be `Illuminate\Database\Eloquent\Model` instance.");
-        }
-
-        $this->model = $model;
+        parent::__construct(config('fintech.promo.promotion_model', Promotion::class));
     }
 
     /**
