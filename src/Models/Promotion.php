c3bb77b2576310492c9483ec4fc59d899a5616a1
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-05-09 12:45:27 +0600

    core message lang file moved to rest api

--- src/Models/Promotion.php
+++ src/Models/Promotion.php
@@ -67,10 +67,10 @@ class Promotion extends BaseModel implements HasMedia
         $primaryKey = $this->getKey();
 
         $links = [
-            'show' => action_link(route('promo.promotions.show', $primaryKey), __('core::messages.action.show'), 'get'),
-            'update' => action_link(route('promo.promotions.update', $primaryKey), __('core::messages.action.update'), 'put'),
-            'destroy' => action_link(route('promo.promotions.destroy', $primaryKey), __('core::messages.action.destroy'), 'delete'),
-            'restore' => action_link(route('promo.promotions.restore', $primaryKey), __('core::messages.action.restore'), 'post'),
+            'show' => action_link(route('promo.promotions.show', $primaryKey), __('restapi::messages.action.show'), 'get'),
+            'update' => action_link(route('promo.promotions.update', $primaryKey), __('restapi::messages.action.update'), 'put'),
+            'destroy' => action_link(route('promo.promotions.destroy', $primaryKey), __('restapi::messages.action.destroy'), 'delete'),
+            'restore' => action_link(route('promo.promotions.restore', $primaryKey), __('restapi::messages.action.restore'), 'post'),
         ];
 
         if ($this->getAttribute('deleted_at') == null) {
