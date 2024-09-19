261fcd300b077b97b0d303469876e8b0462bd591
hafijul233 <hafijul233@users.noreply.github.com>
2024-09-19 20:16:53 +0000

    Fix styling

--- src/Promo.php
+++ src/Promo.php
@@ -7,8 +7,8 @@ use Fintech\Promo\Services\PromotionService;
 class Promo
 {
     public function promotion($filters = null)
-{
-	return \singleton(PromotionService::class, $filters);
+    {
+        return \singleton(PromotionService::class, $filters);
     }
 
     //** Crud Service Method Point Do not Remove **//
