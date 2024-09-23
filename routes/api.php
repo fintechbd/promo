d4a96eae8589c91d0aa5bd177ae3d5e0d5e5d589
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-23 15:04:01 +0600

    platform check middleware added

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 if (Config::get('fintech.promo.enabled')) {
-    Route::prefix(config('fintech.promo.root_prefix', 'api/'))->middleware(['api', 'http_log', 'encrypted'])->group(function () {
+    Route::prefix(config('fintech.promo.root_prefix', 'api/'))->middleware(['api'])->group(function () {
         Route::prefix('promo')->name('promo.')
             ->middleware(config('fintech.auth.middleware'))
             ->group(function () {
